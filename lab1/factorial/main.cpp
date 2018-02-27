41fb21ee2494025ed5c5d87303088174a2808a31
Piotr Walat <p_walat@icloud.com>
2018-02-27 09:27:03 +0100

    commit lab1

--- lab1/factorial/main.cpp
+++ lab1/factorial/main.cpp
@@ -2,6 +2,6 @@
 #include "Factorial.h"
 
 int main() {
-  std::cout << "factorial of 20 is: " << factorial(20) << std::endl;
+  std::cout << "factorial of 12 is: " << factorial(12) << std::endl;
   return 0;
 }
